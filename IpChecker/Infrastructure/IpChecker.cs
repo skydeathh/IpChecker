d7d4c89b337c71c7f7ccc87bfadcebf71230529c
Алексеев Дмитрий <a1b2f2f2@gmail.com>
2023-08-30 22:32:25 +0700

    develop: app created, shows only country symbols

---
+++ IpChecker/Infrastructure/IpChecker.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IpChecker.Infrastructure;
+public static class IpChecker {
+    private static string _url = "http://ipwho.is/";
+    private static HttpClient _httpClient = new();
+    private static string json;
+
+    public static async Task<string> GetJson(string ip) {
+        using var response = await _httpClient.GetAsync(_url + ip);
+        if (response is null) {
+            return null;
+        }
+
+        json = await response.Content.ReadAsStringAsync();
+        return json;
+    }
+
+    public static async Task<string> GetLocation(string ip) {
+        var res = await GetJson(ip);
+        if (JObject.Parse(res)["success"].ToString() == "False")
+            return "Invalid ip";
+
+        return JObject.Parse(res)["country_code"].ToString();
+    }
+}
---
+++ IpChecker/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using IpChecker.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using IpChecker.Infrastructure;
+
+namespace IpChecker.ViewModels;
+public class MainViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private string _location;
+    private string _ip;
+    public ICommand GetLocationCommand { get; }
+
+    public MainViewModel()
+        => GetLocationCommand = new RelayCommand(async ()
+            => await GetLocationAsync());
+
+    public string Location
+    {
+        get { return _location; }
+        set
+        {
+            _location = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string IPAddress
+    {
+        get { return _ip; }
+        set
+        {
+            _ip = value;
+            OnPropertyChanged(nameof(IPAddress));
+        }
+    }
+
+    private async Task GetLocationAsync()
+    {
+        string ip = IPAddress;
+
+        Location = await Infrastructure.IpChecker.GetLocation(ip);
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
